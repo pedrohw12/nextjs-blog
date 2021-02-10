e8963e49b64b90a36576413ccd74028e12eeb47f
Pedro Wanderley <peu-zico@hotmail.com>
2021-02-09 23:24:06 -0300

    first commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,18 @@
 import Nav from "./Nav";
+import Meta from "./Meta";
+import Header from "./Header";
 import styles from "../styles/Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Meta />
       <Nav />
       <div className={styles.container}>
-        <main className={styles.main}>{children}</main>
+        <main className={styles.main}>
+          <Header />
+          {children}
+        </main>
       </div>
     </>
   );
